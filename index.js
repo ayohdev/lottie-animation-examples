d553c23d9c37d63f09faff2a438f0e28f16abe22
Andrew Hewitt <hello@yohdigital.com>
2019-12-08 01:01:23 -0500

    added checkmark animation

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-
-
-
 var animation = bodymovin.loadAnimation({
   container: document.getElementById('bm'),
   renderer: 'svg',
   loop: true,
   autoplay: true,
   path: '/animations/data.json'
-})
+});
+
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('bm-1'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: '/animations/checkmark.json'
+});
